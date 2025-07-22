87ba4f9bd986223685db04cee606ca410ca80fa6
Zoea <71321839+Chenxue3@users.noreply.github.com>
2025-07-22 12:28:48 +1200

    fix loading issue

--- frontend/build/_nuxt/static/1753141506/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpregnancy-app\u002F_nuxt\u002Fstatic\u002F1753141506",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002F",config:{_app:{basePath:"\u002Fpregnancy-app\u002F",assetsPath:"\u002Fpregnancy-app\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- frontend/build/_nuxt/static/1753141506/video/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpregnancy-app\u002F_nuxt\u002Fstatic\u002F1753141506",layout:"default",error:a,state:{currentContent:{}},serverRendered:true,routePath:"\u002Fvideo",config:{_app:{basePath:"\u002Fpregnancy-app\u002F",assetsPath:"\u002Fpregnancy-app\u002F_nuxt\u002F",cdnURL:a}}}}(null));
